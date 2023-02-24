f2a9991d076cdc2f89dc5c561f748e948a2d81a2
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-02-25 00:14:59 +0530

    whole assignment completed

---
+++ index.js
@@ -0,0 +1,509 @@
+
+let data=[
+    {
+Topic:	"Physic",
+
+Question_Serial_No:	1,
+
+Question_Type:	'MCQ',
+
+Subtopic_text:	'AStandard Form of ∫_(0 )^(π/2)▒〖sinx dx 〗R',
+
+Subtopic_img:'',
+
+Instructions:	'Choose the single correct option',
+
+Question_Text:	'The human eye is like a camera. Which of the following acts as a screen for this camera?',
+
+Question_img:'',
+
+Options:[{C:'A',A : 'Retina'},{C:'B',A: 'Cornea'},{C:'C',A:'Iris'},{C:'D',A:'Pupil'}],		
+
+Answers:	'A',
+
+Solution_text:	'The human eye is like a camera. Its lens system forms an image on a light-sensitive screen called the retina',
+
+Solution_img:'',
+
+Exams:	'CBSE',
+
+Concept_Tips:	['https://www.youtube.com/watch?v=uuWb1L2Vwsk/', 'https://en.wikipedia.org/wiki/Human_eye'],
+
+Taxonomy:	'Conceptual',
+
+Difficulty: 'Easy',
+
+PYQ:	'JEE - 1999/JEE - 2004',
+
+Wow_Factor:	'Yes',
+
+Quality_Grade:	'A',
+
+Is_Guided:	'Yes',
+
+Primary_Concept:	'Eye',
+
+Secondary_Concept:	'Human eye',
+
+Remarks:'Need Improvement'	 
+
+},
+{
+
+    Topic:	"Biology",
+    
+    Question_Serial_No:	2,
+    
+    Question_Type:	'MCQ',
+    
+    Subtopic_text:	'(lim)┬(n→∞)⁡〖(1+1/n)^n 〗 Limit',
+    
+    Subtopic_img:'',
+    
+    Instructions:	'Choose the single correct option',
+    
+    Question_Text:	'Which of the following is the function of the human liver?',
+    
+    Question_img:'https://i.postimg.cc/FsKmBKgt/image.png',
+    
+    Options:[{C:'A',A : 'Production of bile'},{C:'B',A: 'Metabolization of fats'},{C:'C',A:'Metabolization of carbohydrates'},{C:'D',A:'Production of blood proteins'}],
+                
+    Answers:	'A, B, C',
+    
+    Solution_text:	'The human eye is like a camera. Its lens system forms an image on a light-sensitive screen called the retina',
+    
+    Solution_img:'https://i.postimg.cc/bwTRRM2L/image.png',
+    
+    Exams:	'CBSE',
+    
+    Concept_Tips:	['',''],
+    
+    Taxonomy:	'',
+    
+    Difficulty: 'Easy',
+    
+    PYQ:	'JEE - 1999/JEE – 2004',
+    
+    Wow_Factor:	'Yes',
+    
+    Quality_Grade:	'A',
+    
+    
+    Is_Guided:	'Yes',
+    
+    Primary_Concept:	'',
+    
+    Secondary_Concept:	'text/pdf/video link',
+    
+    Remarks:'Need Improvement'
+         
+    },
+
+    {
+
+        Topic:	"Biology",
+        
+        Question_Serial_No:	3,
+        
+        Question_Type:	'SCQ',
+        
+        Subtopic_text:	'',
+        
+        Subtopic_img:'',
+        
+        Instructions:	'<General instructions about the question>',
+        
+        Question_Text:	'Checking Font, Checking Font, Checking Font The IUPAC name of the following is:',
+        
+        Question_img:'https://i.postimg.cc/qBcsXSKY/image.png',
+        
+        Options:[{C:'A',A : '3-Bromo-5-chlorononane'},
+        {C:'B',A: '5-Bromo-7-chlorononane'},
+        {C:'C',A:'5-Chloro-3-bromononane'},
+        {C:'D',A:'7-Chloro-5-bromononane'}],
+                    
+        Answers:	'C',
+        
+        Solution_text:	'The longest carbon chain contains 9 carbons, and all the carbon atoms are connected by single bond. Thus, the parent chain will be named as nonane. To give the lowest possible number to the halo groups attached to the chain, numbering will be done from wight hand side. So, Bromo (-Br) group is attached at third carbon and chloro group (-Cl) is attached at fifth carbon. According to IUPAC naming rule, alphabetical order should be followed. Thus, the correct IUPAC name of the compound is 3-Bromo-5-chlorononane.',
+        
+        Solution_img:'https://i.postimg.cc/PJcT82dM/image.png',
+        
+        Exams:	'IIT',
+        
+        Concept_Tips:	['',''],
+        
+        Taxonomy:	'Conceptual',
+        
+        Difficulty: 'Moderate',
+        
+        PYQ:	'JEE - 1999/JEE – 2004 , CBSE-2021 / CBSE - 1998',
+        
+        Wow_Factor:	'Yes',
+        
+        Quality_Grade:	'A',
+        
+        
+        Is_Guided:	'Yes',
+        
+        Primary_Concept:	'',
+        
+        Secondary_Concept:	'g-drive.spreadsheet Secondary Concept	text/pdf/video link',
+        
+        Remarks:'Need Improvement'
+             
+        },
+
+        {
+
+            Topic:	"Biology",
+            
+            Question_Serial_No:	4,
+            
+            Question_Type:	'MCQ',
+            
+            Subtopic_text:	'Atmosphere',
+            
+            Subtopic_img:'',
+            
+            Instructions:	'Two statements are given, one labelled Assertion (A) and the other labelled Reason (R). Select the correct answer to these questions from the codes (A), (B), (C) and (D) as',
+            
+            Question_Text:	'Assertion (A): When white light enters a prism, it gets split into its constituent colours. Reason (R): Different varieties of glass have different refractive indices.',
+            
+            Question_img:'https://i.postimg.cc/sfw5BS7G/image.png',
+            
+            Options:[{C:'A',A : 'Both A and R are true and R is the correct explanation of assertion.'},
+            {C:'B',A: 'Both A and R are true and R is not the correct explanation of the assertion.'},
+            {C:'C',A:'A is true but R is false.'},
+            {C:'D',A:'A is false but R is true.'}],
+                        
+            Answers:	'B',
+            
+            Solution_text:	'When white light enters a prism, it gets split into its constituent colours. Hence, Assertion (A) is true. Different varieties of glass have different refractive indices. Hence, Reason (R) is true. But the reason is not the correct explanation of the assertion. BrF_5=AX_6  type molecule,Number of bond pair=5 and number of lone pair=1. Expected geometry-Octahedral and actual-Square pyramidal. ICl4- = AX_6  type molecule,Number of bond pair=4 and number of lone pair=2. Expected geometry-Octahedral and actual-Square planar. SnCl2 = AX_3  type molecule,Number of bond pair=2 and number of lone pair=1. Expected geometry-Trigonal planar and actual-V-shape or bent or angular. XeF6 =AX_7  type molecule,Number of bond pair=6 and number of lone pair=1. Expected geometry-Pentagonal pyramidal and actual-Distorted octahedral.',
+            
+            Solution_img:'',
+            
+            Exams:	'IIT',
+            
+            Concept_Tips:	['<external link>',''],
+            
+            Taxonomy:	'Conceptual',
+            
+            Difficulty: 'Easy',
+            
+            PYQ:	'JEE - 1999/JEE – 2004',
+            
+            Wow_Factor:	'Yes',
+            
+            Quality_Grade:	'A',
+            
+            
+            Is_Guided:	'Yes',
+            
+            Primary_Concept:	'',
+            
+            Secondary_Concept:	'text/pdf/video link',
+            
+            Remarks:'Need Improvement'
+                 
+            }
+    
+
+]
+apppedData(data);
+
+function apppedData(data)
+{
+    document.getElementById('container').innerHTML='';
+    data.forEach((ele)=>{
+
+let quediv = document.createElement('div');
+
+//1
+let Topic=document.createElement('div');
+
+let topicKey=document.createElement('div')
+topicKey.innerText='Topic';
+
+let topicValue=document.createElement('div')
+topicValue.innerText=ele.Topic;
+
+Topic.append(topicKey,topicValue);
+
+//2
+let Question_Serial_No=document.createElement('div');
+
+let Question_Serial_NoKey=document.createElement('div');
+Question_Serial_NoKey.innerText='Question Serial No';
+
+let Question_Serial_NoValue=document.createElement('div');
+Question_Serial_NoValue.innerText=ele.Question_Serial_No;
+
+Question_Serial_No.append(Question_Serial_NoKey,Question_Serial_NoValue);
+
+//3
+let Question_Type=document.createElement('div');
+
+let Question_TypeKey=document.createElement('div');
+Question_TypeKey.innerText='Question Type';
+
+let Question_TypeValue=document.createElement('div')
+Question_TypeValue.innerText=ele.Question_Type;
+
+Question_Type.append(Question_TypeKey,Question_TypeValue);
+
+//4
+let Subtopic_text= document.createElement('div');
+
+let Subtopic_textKey=document.createElement('div')
+Subtopic_textKey.innerText='Subtopic';
+
+let Subtopic_textValue=document.createElement('div')
+Subtopic_textValue.innerText=ele.Subtopic_text;
+
+
+//5 img 
+let Subtopic_img= document.createElement('img');
+
+Subtopic_img.src=ele.Subtopic_img;
+
+Subtopic_textValue.append(Subtopic_img);
+
+Subtopic_text.append(Subtopic_textKey,Subtopic_textValue);
+
+
+//6
+let Instructions= document.createElement('div');
+
+let InstructionsKey= document.createElement('div')
+InstructionsKey.innerText='Instructions';
+
+let InstructionsValue= document.createElement('div')
+InstructionsValue.innerText=ele.Instructions;
+
+Instructions.append(InstructionsKey,InstructionsValue);
+
+//7
+let Question_Text= document.createElement('div');
+let Question_TextKey= document.createElement('div')
+Question_TextKey.innerText='Question Text'
+
+let Question_TextValue= document.createElement('div')
+Question_TextValue.innerText=ele.Question_Text;
+
+
+//8 img
+let Question_img= document.createElement('img');
+Question_img.src=ele.Question_img;
+
+Question_TextValue.append(Question_img);
+
+Question_Text.append(Question_TextKey,Question_TextValue);
+
+Question_Text.append(Question_TextKey,Question_TextValue);
+
+
+//9 options
+let Options= document.createElement('div');
+let OptionsKey= document.createElement('div');
+OptionsKey.innerText='Options';
+let OptionsValue= document.createElement('div');
+
+OptionsValue.setAttribute('id','option');
+
+let arrObj=ele.Options;
+
+arrObj.forEach((e)=>{
+    let parent=document.createElement('div');
+    let key=document.createElement('div');
+    key.innerText=e.C;
+    let value=document.createElement('div');
+    value.innerText=e.A;
+    parent.append(key,value);
+    OptionsValue.append(parent);
+});
+
+Options.append(OptionsKey,OptionsValue);
+//10
+let Answers= document.createElement('div');
+
+let ansKey=document.createElement('div')
+ansKey.innerText="Answer";
+
+let ansValue=document.createElement('div')
+ansValue.innerText=ele.Answers;
+
+Answers.append(ansKey,ansValue);
+
+//11
+let Solution_text= document.createElement('div');
+
+let Solution_textKey= document.createElement('div')
+Solution_textKey.innerText='Solution';
+
+let Solution_textValue= document.createElement('div')
+Solution_textValue.innerText=ele.Solution_text;
+
+
+
+//12 img
+let Solution_img= document.createElement('img');
+
+Solution_img.src=ele.Solution_img;
+
+Solution_textValue.append(Solution_img);
+
+Solution_text.append(Solution_textKey,Solution_textValue);
+
+//13
+let Exams= document.createElement('div');
+
+let ExamsKey= document.createElement('div')
+ExamsKey.innerText='Exams';
+
+let ExamsValue= document.createElement('div')
+ExamsValue.innerText=ele.Exams;
+
+Exams.append(ExamsKey,ExamsValue);
+
+//14
+let Concept_Tips= document.createElement('div');
+
+let Concept_TipsKey= document.createElement('div')
+Concept_TipsKey.innerText='Concept Tips';
+
+let Concept_TipsValue= document.createElement('div')
+arr=ele.Concept_Tips;
+arr.forEach((e)=>{
+let a=document.createElement('a');
+a.href=e;
+a.innerText=e;
+Concept_TipsValue.append(a);
+});
+
+Concept_Tips.append(Concept_TipsKey,Concept_TipsValue);
+
+//15
+let Taxonomy= document.createElement('div');
+let TaxonomyKey= document.createElement('div')
+TaxonomyKey.innerText='Taxonomy';
+let TaxonomyValue= document.createElement('div')
+TaxonomyValue.innerText=ele.Taxonomy;
+Taxonomy.append(TaxonomyKey,TaxonomyValue);
+
+//16
+let Difficulty= document.createElement('div');
+let DifficultyKey= document.createElement('div')
+DifficultyKey.innerText='Difficulty Level';
+let DifficultyValue= document.createElement('div')
+DifficultyValue.innerText=ele.Difficulty;
+Difficulty.append(DifficultyKey,DifficultyValue);
+
+//17
+let PYQ= document.createElement('div');
+let PYQKey= document.createElement('div')
+PYQKey.innerText='PYQ'
+let PYQValue= document.createElement('div')
+PYQValue.innerText=ele.PYQ;
+PYQ.append(PYQKey,PYQValue);
+
+//18
+let Wow_Factor= document.createElement('div');
+let Wow_FactorKey= document.createElement('div')
+Wow_FactorKey.innerText='Wow Factor'
+let Wow_FactorValue= document.createElement('div')
+Wow_FactorValue.innerText=ele.Wow_Factor;
+Wow_Factor.append(Wow_FactorKey,Wow_FactorValue);
+
+
+//19
+let Quality_Grade= document.createElement('div');
+let Quality_GradeKey= document.createElement('div')
+Quality_GradeKey.innerText='Quality Grade'
+let Quality_GradeValue= document.createElement('div')
+Quality_GradeValue.innerText=ele.Quality_Grade;
+Quality_Grade.append(Quality_GradeKey,Quality_GradeValue);
+
+//20
+let Is_Guided= document.createElement('div');
+let Is_GuidedKey= document.createElement('div')
+Is_GuidedKey.innerText='Is Guided';
+let Is_GuidedValue= document.createElement('div')
+Is_GuidedValue.innerText=ele.Is_Guided;
+Is_Guided.append(Is_GuidedKey,Is_GuidedValue);
+
+
+//21  link or text
+let Primary_Concept= document.createElement('div');
+let Primary_ConceptKey= document.createElement('div')
+Primary_ConceptKey.innerText='Primary Concept';
+let Primary_ConceptValue= document.createElement('div')
+Primary_ConceptValue.innerText=ele.Primary_Concept;
+Primary_Concept.append(Primary_ConceptKey,Primary_ConceptValue);
+
+//22 line or text
+let Secondary_Concept= document.createElement('div');
+let Secondary_ConceptKey= document.createElement('div')
+Secondary_ConceptKey.innerText='Secondary Concept';
+let Secondary_ConceptValue= document.createElement('div')
+Secondary_ConceptValue.innerText=ele.Secondary_Concept;
+Secondary_Concept.append(Secondary_ConceptKey,Secondary_ConceptValue);
+
+//23 Text
+let Remarks= document.createElement('div'); 
+let RemarksKey= document.createElement('div')
+RemarksKey.innerText='Remarks';
+let RemarksValue= document.createElement('div')
+RemarksValue.innerText=ele.Remarks;
+Remarks.append(RemarksKey,RemarksValue) ;
+
+
+quediv.append(
+    Topic,
+    Question_Serial_No,
+    Question_Type,
+    Subtopic_text, // 5 img
+    Instructions,
+    Question_Text,
+    Options, // 8 img ,9 options
+    Answers,
+    Solution_text, // 12 img
+    Exams,
+    Concept_Tips,
+    Taxonomy,
+    Difficulty,
+    PYQ,
+    Wow_Factor,
+    Quality_Grade,
+    Is_Guided,
+    Primary_Concept,
+    Secondary_Concept,
+    Remarks
+
+    );
+    document.getElementById('container').append(quediv);
+
+    });
+}
+
+let filter=document.querySelector('#filterByDL');
+
+filter.addEventListener('change',doFilter);
+
+function doFilter()
+{
+    if(filter.value!="")
+    {
+        let arr=data.filter((el,i)=>{
+                  
+           return el.Difficulty==filter.value;
+
+
+        });
+
+        apppedData(arr);
+
+    }
+    else{
+        apppedData(data);
+    }
+}
